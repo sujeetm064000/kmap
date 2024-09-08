5161c715de6f534b35022bab67b4b72708e6c3a1
sujeet kumar <kannaujiasujeetkumar@gmail.com>
2024-09-08 18:12:27 +0530

    added update

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
     const rfid = document.getElementById('rfid').value;
@@ -11,23 +11,33 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         },
         body: JSON.stringify({ rfid, password }),
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            // Save user info to local storage
-            localStorage.setItem('user', JSON.stringify({
-                name: data.user_name,
-                rfid: data.rfid
-            }));
-            alert(data.message);
-            window.location.href = 'index.html'; // Redirect to the map page
-        } else {
-            alert('Invalid RFID or password');
-        }
-    })
-    .catch(error => console.error('Error:', error));
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                // Save user info to local storage
+                localStorage.setItem('user', JSON.stringify({
+                    name: data.user_name,
+                    rfid: data.rfid
+                }));
+                alert(data.message);
+                window.location.href = 'index.html'; // Redirect to the map page
+            } else {
+                alert('Invalid RFID or password');
+            }
+        })
+        .catch(error => console.error('Error:', error));
 });
 
-document.getElementById('joinButton').addEventListener('click', function() {
+document.getElementById('joinButton').addEventListener('click', function () {
     window.location.href = 'add-user.html'; // Redirect to the add user page
+});
+document.addEventListener('DOMContentLoaded', function () {
+    let userdetails = window.localStorage.getItem("userdetails");
+    if (userdetails == null) {
+        const joinBtn = document.getElementById("joinButton");
+        joinBtn.style.display = "block";
+    }
+    else{
+        joinBtn.style.display = "none";
+    }
 });
